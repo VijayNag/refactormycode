52879d505e8588bb4d9221875c13a6f50e416be6
Martin Klose <martin.klose@klosebrothers.de>
2011-11-25 18:37:34 +0100

    extract next test and replace comment with descriptive method name

--- java/string_calculator/src/test/java/StringCalculatorTest.java
+++ java/string_calculator/src/test/java/StringCalculatorTest.java
@@ -8,8 +8,6 @@ public class StringCalculatorTest {
 
 	@Test
 	public void testCalculator() throws Exception {
-		assertEquals(2356, s.add("2341,15")); // should add comma separated
-												// numbers
 		assertEquals(35, s.add("34\n1")); // should treat new line as separator
 		assertEquals(7, s.add("//;\n5;2")); // should support custom separators
 	}
@@ -18,9 +16,14 @@ public class StringCalculatorTest {
 	public void emptyStringAddsUpToZero() throws Exception {
 		assertEquals(0, s.add(""));
 	}
-	
+
 	@Test
 	public void addUpSingleNumber() throws Exception {
 		assertEquals(17, s.add("17"));
+	}
+
+	@Test
+	public void addUpTwoNumbersSeparatedWithComma() throws Exception {
+		assertEquals(2356, s.add("2341,15"));
 	}
 }
