c0e25c80b25973fb64e8c165d0fbaacf81b2a230
Martin Klose <martin.klose@klosebrothers.de>
2011-11-25 18:27:42 +0100

    remove duplication

--- java/string_calculator/src/test/java/StringCalculatorTest.java
+++ java/string_calculator/src/test/java/StringCalculatorTest.java
@@ -2,23 +2,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class StringCalculatorTest {
+
+	StringCalculator s = new StringCalculator();
 
 	@Test
 	public void testCalculator() throws Exception {
-		StringCalculator s = new StringCalculator();
 		assertEquals(17, s.add("17")); // should add single numbers
 		assertEquals(2356, s.add("2341,15")); // should add comma separated
 												// numbers
 		assertEquals(35, s.add("34\n1")); // should treat new line as separator
 		assertEquals(7, s.add("//;\n5;2")); // should support custom separators
 	}
-	
+
 	@Test
 	public void testname() throws Exception {
-		StringCalculator s = new StringCalculator();
 		assertEquals(0, s.add("")); // should treat empty string as zero
-		
+
 	}
 }
