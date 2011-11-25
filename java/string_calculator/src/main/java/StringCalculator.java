ff889f873c29a1bac432d590bdd3981e3230fbe1
Martin Klose <martin.klose@klosebrothers.de>
2011-11-25 18:51:38 +0100

    swap lines to separated summing up code from extracting numbers

--- java/string_calculator/src/main/java/StringCalculator.java
+++ java/string_calculator/src/main/java/StringCalculator.java
@@ -4,15 +4,15 @@ public class StringCalculator {
 			return 0;
 		}
 		if (s.startsWith("//")) {
-			int result = 0;
 			String[] numbers = s.substring(4).split("[\n" + s.charAt(2) + "]");
+			int result = 0;
 			for (String n : numbers) {
 				result += Integer.parseInt(n);
 			}
 			return result;
 		} else {
-			int result = 0;
 			String[] numbers = s.split("[\n,]");
+			int result = 0;
 			for (String n : numbers) {
 				result += Integer.parseInt(n);
 			}
