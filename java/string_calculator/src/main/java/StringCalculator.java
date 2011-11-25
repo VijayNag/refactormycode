51a1a99a69c5fffe8e124192d36cebcb0e867cc2
Martin Klose <martin.klose@klosebrothers.de>
2011-11-25 19:08:09 +0100

    remove unnecessary brackets

--- java/string_calculator/src/main/java/StringCalculator.java
+++ java/string_calculator/src/main/java/StringCalculator.java
@@ -1,15 +1,13 @@
 public class StringCalculator {
 	public int add(String s) {
-		if (s.isEmpty()) {
+		if (s.isEmpty())
 			return 0;
-		}
 		return sum(extractNumbers(s));
 	}
 
 	private String[] extractNumbers(String s) {
-		if (hasCustomSeparator(s)) {
+		if (hasCustomSeparator(s))
 			return s.substring(4).split("[\n" + s.charAt(2) + "]");
-		}
 		return s.split("[\n,]");
 	}
 
@@ -19,9 +17,8 @@ public class StringCalculator {
 
 	private int sum(String[] numbers) {
 		int result = 0;
-		for (String n : numbers) {
+		for (String n : numbers)
 			result += Integer.parseInt(n);
-		}
 		return result;
 	}
 }
